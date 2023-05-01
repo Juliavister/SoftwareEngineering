b69e3113fbcbfe6473f2906a63bba1ecc688c912
Doğa Kurtoğlu <71259401+ehady@users.noreply.github.com>
2023-05-01 17:18:52 +0200

    Update db.py

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ import pyodbc
 
 server = 'dogakserver.database.windows.net'
 database = 'Currency'
-username = 'doga123'
-password = 'Ed44ha03'
+username = ''
+password = ''
 driver= '{ODBC Driver 17 for SQL Server}'
 cnxn = pyodbc.connect(f'DRIVER={driver};SERVER={server};PORT=1433;DATABASE={database};UID={username};PWD={password}')
 cursor = cnxn.cursor()
